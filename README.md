175532584eca9651f9d0a69c357471c514bec00d
Anton Romanov <romanov73@gmail.com>
2016-09-23 14:34:39 +0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# java-server-socket-example
+At start server waits client connetion. 
+
+1. Compile maven project
+2. Go to "target" dir
+2. Run as: java -jar server-0.1.0-SNAPSHOT.jar
---
+++ src/main/java/ru/ulstu/server/Main.java
@@ -0,0 +1,50 @@
+package ru.ulstu.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+
+    public static final int PORT = 6666; // случайный порт (может быть любое число от 1025 до 65535)
+    
+    public static final String IP = "127.0.0.1";
+
+    public static void main(String[] args) {
+
+        try {
+            InetAddress ipAddress = InetAddress.getByName(IP);
+            ServerSocket ss = new ServerSocket(PORT, 0, ipAddress);
+            System.out.println("Waiting for a client...");
+
+            Socket socket = ss.accept(); // заставляем сервер ждать подключений и выводим сообщение когда кто-то связался с сервером
+            System.out.println("Got a client :) ... Finally, someone saw me through all the cover!");
+            System.out.println();
+
+            // Берем входной и выходной потоки сокета, теперь можем получать и отсылать данные клиенту. 
+            InputStream sin = socket.getInputStream();
+            OutputStream sout = socket.getOutputStream();
+
+            // Конвертируем потоки в другой тип, чтоб легче обрабатывать текстовые сообщения.
+            DataInputStream in = new DataInputStream(sin);
+            DataOutputStream out = new DataOutputStream(sout);
+
+            String line = null;
+            while (true) {
+                line = in.readUTF(); // ожидаем пока клиент пришлет строку текста.
+                System.out.println("The dumb client just sent me this line : " + line);
+                System.out.println("I'm sending it back...");
+                out.writeUTF(line); // отсылаем клиенту обратно ту самую строку текста.
+                out.flush(); // заставляем поток закончить передачу данных.
+                System.out.println("Waiting for the next line...");
+                System.out.println();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
